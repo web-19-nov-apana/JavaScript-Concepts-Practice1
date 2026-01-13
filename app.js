09caa728dca90b763f7819cb3e89f305381feff4
MITHLESH KUMAR <mithleshkumar852144@gmail.com>
2026-01-13 18:55:22 +0530

    Fundamental Concepts

---
+++ app.js
@@ -0,0 +1,236 @@
+// console.log("hello world");
+// console.log(1 + 2);
+let num = 13;
+let num2 = 23;
+console.log("Sum is :", num + num2);
+
+// Template literal
+
+// let a = 3;
+// let b = 8;
+// let ouput = `sum is : ${a + b} total`
+// console.log(ouput);
+
+// Operator
+
+// Arithemic operator
+
+// let d = 2;
+// let c = 4;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+// Unary operator
+
+// let B = 2;
+// let C = 4;
+// console.log(B++);
+// console.log(++B);
+
+// Assegment operator
+
+// let a = 2;
+// let b = 8;
+// a = b;
+// console.log(a);//8
+
+// ~Compersion opertaor
+
+// let age = 12;
+// console.log(age >= 18);
+// console.log(age <= 23);
+
+// ~ Conditional statement
+
+// console.log("befor mi if condition");
+
+// let age = 23;
+// if (age >= 18)
+// {
+//     console.log("you can vote");
+//     console.log("you can drive");
+
+    
+// }
+// console.log("After if statement");
+
+// ~ Practice question
+
+// let color = "yellow";
+// if (color == "red")
+// {
+//     console.log("Stop");
+    
+// }
+// if (color == "yellow") {
+//     console.log("Warning");
+
+// }
+// if (color == "green") {
+//     console.log("Go");
+
+// }
+
+// ~ else if statment
+
+// let age = 12;
+// if (age >= 18)
+// {
+//     console.log("you can vote");
+    
+// }
+// else if (age <= 18)
+// {
+//     console.log("you can not vote");
+    
+// }
+
+// let mark = 12;
+// if (mark >=80)
+// {
+//    console.log("A+");
+   
+// } else if (mark >= 60)
+// {
+//     console.log("A");
+    
+// }
+// else if (mark >= 33)
+// {
+//     console.log("B");
+    
+// }
+// else if (mark < 33)
+// {
+//     console.log("fail");
+    
+// }
+
+// ~ if else statment
+
+// let age = 13;
+// if (age >= 18) {
+//     console.log("you can vote");
+    
+// } else {
+//     console.log("you can not vote");
+    
+// }
+
+// ~ Practice question
+
+// let size = "S";
+
+// if (size == "XL")
+// {
+//     console.log("Price is Rs. 250");
+    
+// } else if (size == "L")
+// {
+//     console.log("Price is Rs. 200");
+    
+// }
+// else if (size == "M")
+// {
+//     console.log("Price is Rs. 100");
+    
+// } else {
+//     console.log("Price is Rs. 50");
+    
+// }
+
+// ~Pratcice question question 1
+
+// let str = "pple";
+
+// if ((str[0] === 'a') && (str.length > 3))
+// {
+//     console.log("Good string");
+    
+// } else {
+//     console.log("Not good string");
+    
+// }
+
+// ~Truthy and falsey value,falsey value=0,-0,empty string,null,undefined,NaN,On(Bigint). and Truthy value=1,[],{}
+
+// if (1) {
+//     console.log("hii");
+    
+// } else {
+//     console.log("bad");
+    
+// }
+
+// ~ Switch case statement
+
+// let color = "kion";
+
+// switch (color) {
+//     case "red":
+//         console.log("stop");
+//         break;
+//     case "yellow":
+//         console.log("warning");
+//         break;
+//     case "green":
+//         console.log("go");
+//         break;
+//     default:
+//         console.log("trafice has broken");
+        
+        
+// }
+
+// ~ Practice question
+
+// let day = 6;
+
+// switch (day) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturaday");
+//         break;
+//     case 7:
+//         console.log("Sunday");
+//         break;
+//     default:
+//         console.log("this is not week number");
+
+
+// }
+
+// ~Alert and Prompt
+
+// alert(Danger)
+
+let firstname = prompt("Enter the first name");
+console.log(firstname);
+
+
+
+
+
+
+
+
+
+
